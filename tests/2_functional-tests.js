f79f6e8e172799ec41b6837cb10deb8125c46735
ali m alghanay <a.al-ghanay@uot.edu.ly>
2023-10-25 15:30:04 +0200

    should pass chanllge

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,26 +16,19 @@ let id;
 let wrongId = "653140d16f8e00089c6177a2";
 suite('Functional Tests', function() {
 
-  /*
-  * ----[EXAMPLE TEST]----
-  * Each test should completely test the response of the API end-point including response status code!
-  */
-  // test('#example Test GET /api/books', function(done){
-  //    chai.request(server)
-  //     .get('/api/books')
-  //     .end(function(err, res){
-  //       assert.equal(res.status, 200);
-  //       assert.isArray(res.body, 'response should be an array');
-  //       assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
-  //       assert.property(res.body[0], 'title', 'Books in array should contain title');
-  //       assert.property(res.body[0], '_id', 'Books in array should contain _id');
-  //       done();
-  //     });
-  // });
-  /*
-  * ----[END of EXAMPLE TEST]----
-  */
-
+ test('#example Test GET /api/books', function(done){
+     chai.request(server)
+      .get('/api/books')
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'response should be an array');
+        assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+        assert.property(res.body[0], 'title', 'Books in array should contain title');
+        assert.property(res.body[0], '_id', 'Books in array should contain _id');
+        done();
+      });
+  });
+  
   suite('Routing tests', function() {
 
 
